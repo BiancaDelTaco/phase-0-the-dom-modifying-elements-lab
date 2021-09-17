0eb9a0c2927af622b6aa6893ecb435541282d364
Nicole Gage <nicole.m.gage@gmail.com>
2021-09-17 22:11:53 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove("main");
+
+
+const newHeader = document.createElement("h1")
+    newHeader.setAttribute("id", "victory")
+    
+   newHeader.innerHTML = "Nicole is the champion"
+   
+   document.body.append(newHeader)
+
+
